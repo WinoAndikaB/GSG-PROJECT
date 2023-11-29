d3624ff7d724f2fe609b941bd1dd87679f21977f
WinoAndikaB <107344071+WinoAndikaB@users.noreply.github.com>
2023-11-29 17:26:37 +0700

    [Update 98.1]

--- database/migrations/2023_11_29_102220_modify_like_komentar_artikels_table.php
+++ database/migrations/2023_11_29_102220_modify_like_komentar_artikels_table.php
@@ -4,16 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ModifyLaporanKomentarArtikelsTable extends Migration
+class ModifyLikeKomentarArtikelsTable extends Migration
 {
     public function up()
     {
-        Schema::table('laporan_komentar_artikels', function (Blueprint $table) {
+        Schema::table('like_komentar_artikels', function (Blueprint $table) {
             // Drop the existing foreign key constraint
-            $table->dropForeign('laporan_komentar_artikels_comment_id_foreign');
+            $table->dropForeign('like_komentar_artikels_komentar_id_foreign');
 
             // Add a new foreign key constraint with cascade delete
-            $table->foreign('comment_id')
+            $table->foreign('komentar_id')
                   ->references('id')->on('komentar_artikels')
                   ->onDelete('cascade');
         });
@@ -21,12 +21,12 @@ class ModifyLaporanKomentarArtikelsTable extends Migration
 
     public function down()
     {
-        Schema::table('laporan_komentar_artikels', function (Blueprint $table) {
+        Schema::table('like_komentar_artikels', function (Blueprint $table) {
             // Drop the foreign key constraint in the down method
-            $table->dropForeign('laporan_komentar_artikels_comment_id_foreign');
-            
+            $table->dropForeign('like_komentar_artikels_komentar_id_foreign');
+
             // Recreate the foreign key without cascade delete if needed
-            // $table->foreign('comment_id')
+            // $table->foreign('komentar_id')
             //       ->references('id')->on('komentar_artikels');
         });
     }
